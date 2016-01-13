b9c123113c581bff1b80de25cb10c50f2ca8de8a
Taylor McClure <taylorsmcclure@gmail.com>
2016-01-12 19:01:53 -0800

    Update ec2-socks5.ps1
    Added a few lines to force update the Internet Options proxy settings. Credit to user jvdp81 from the comments of http://blogs.msdn.com/b/aymerics_blog/archive/2013/05/18/scripting-toggle-proxy-server-in-ie-settings-with-powershell.aspx

--- ec2-socks5.ps1
+++ ec2-socks5.ps1
@@ -8,22 +8,33 @@ $putty=""
 $profile=""
 # Input your AWS region here
 $region=""
+$wininet = Add-Type -memberDefinition $source -passthru -name InternetSettings
+$source=@"
 
-# Initializes AWS PS from the default install location
+[DllImport("wininet.dll")]
+
+public static extern bool InternetSetOption(int hInternet, int dwOption, int lpBuffer, int dwBufferLength);  
+
+"@
+
+# Import AWS for PS and loads profile and region
 Import-Module "C:\Program Files (x86)\AWS Tools\PowerShell\AWSPowerShell\AWSPowerShell.psd1"
-# Loads AWS credentials from profile and sets region
 Initialize-AWSDefaults -ProfileName $profile -Region $region
 
 Write-Host "1. Start SOCKS5 EC2 Instance `n
 2. Stop SOCKS5 EC2 Instance `n"
+
 $response = Read-Host -Prompt "1 or 2: "
 
-# If condition to check for 1 or 2, then act appropriately
 If ($response -eq "1")
 {
 	Start-EC2Instance -InstanceIds $ins
 	Write-Host "`nTurning on Proxy settings in Internet Settings..."
 	Set-ItemProperty -path $regKey ProxyEnable -value 1
+    #INTERNET_OPTION_PROXY_SETTINGS_CHANGED
+    $wininet::InternetSetOption([IntPtr]::Zero, 95, [IntPtr]::Zero, 0)|out-null
+    #INTERNET_OPTION_REFRESH
+    $wininet::InternetSetOption([IntPtr]::Zero, 37, [IntPtr]::Zero, 0)|out-null
 	Write-Host "Launching Putty..."
 	Invoke-Item $putty
 	Write-Host "Your instance '$ins' is starting, please wait till it is running."
@@ -33,6 +44,10 @@ ElseIf ($response -eq "2")
 {
 	Stop-EC2Instance -Instance $ins
 	Set-ItemProperty -path $regKey ProxyEnable -value 0
+    #INTERNET_OPTION_PROXY_SETTINGS_CHANGED
+    $wininet::InternetSetOption([IntPtr]::Zero, 95, [IntPtr]::Zero, 0)|out-null
+    #INTERNET_OPTION_REFRESH
+    $wininet::InternetSetOption([IntPtr]::Zero, 37, [IntPtr]::Zero, 0)|out-null
 	Write-Host "`nYour instance '$ins' is stopping..."
 	Start-Sleep -s 5
 }
